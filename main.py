0657db502094c3948f5ea409d821687ee3bf2b7b
Decan29 <58521030+Decan29@users.noreply.github.com>
2024-03-14 14:24:33 +0100

    GPS code wieder eingelagert

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 """Programm für Ankeralarm-App"""
 
 import os
-import json
+# import json
 import math
-from pathlib import Path
+# from pathlib import Path
 from random import random
 
 from kivy.clock import Clock
@@ -21,7 +21,6 @@ from kivymd.uix.dialog import MDDialog
 from kivymd.uix.button import MDFlatButton
 from kivymd.uix.boxlayout import MDBoxLayout
 
-import src.ankeralarm_gps as gps
 import src.settings_controls as sc
 import src.d_pad as d_pad
 
@@ -48,51 +47,47 @@ class MainApp(MDApp):
     #region GPS
     def get_permission(self, dt):
         """Holt Berechtigung für GPS"""
-        # if platform == 'android':
-        #     from android.permissions import Permission, request_permissions
-        #     permissions = [Permission.ACCESS_COARSE_LOCATION, Permission.ACCESS_FINE_LOCATION]
-        #     request_permissions(permissions, self.permission_callback)
-        #     return True
+        if platform == 'android':
+            from android.permissions import Permission, request_permissions
+            permissions = [Permission.ACCESS_COARSE_LOCATION, Permission.ACCESS_FINE_LOCATION]
+            request_permissions(permissions, self.permission_callback)
+            return True
         
-        # return False
-        gps.get_permission(self, dt)
+        return False
 
     def permission_callback(self, permissions, results):
         """Callback-Funktion für Berechtigungen."""
-        # if all(results):
-        #     print("Rechte erteilt")
-        #     self.get_gps()
-        # else:
-        #     print("Rechte abgelehnt")
-        gps.permission_callback(self, permissions, results)
+        if all(results):
+            print("Rechte erteilt")
+            self.get_gps()
+        else:
+            print("Rechte abgelehnt")
 
     def get_gps(self, *args):
         """Holt GPS-Daten mithilfe von plyer"""
-        # if platform == 'android':
-        #     from plyer import gps   
-        #     try:                           
-        #         gps.configure(on_location=self.on_location)
-        #         gps.start(minTime=100, minDistance=0)
-        #     except:
-        #         import traceback
-        #         traceback.print_exc()
-        #         self.gps_status= "GPS is not implemented for your platform"
-        gps.get_gps(self, args)
+        if platform == 'android':
+            from plyer import gps   
+            try:                           
+                gps.configure(on_location=self.on_location)
+                gps.start(minTime=100, minDistance=0)
+            except:
+                import traceback
+                traceback.print_exc()
+                self.gps_status= "GPS is not implemented for your platform"
 
     def on_location(self, **kwargs):
         """Holt lat und lon Werte des aktuellen Standorts. 
         Zudem fügt es diese beim Start der Mapview hinzu."""
-        # self.gps_latitude = kwargs.get('lat', None) 
-        # self.gps_longitude = kwargs.get('lon', None)
-        # if hasattr(self,'gps_latitude') and hasattr(self,'gps_longitude'):
-        #     if self.use_once:
-        #         if  self.root.ids.mapview.lat and self.root.ids.mapview.lon:
-        #             self.root.ids.mapview.lat = self.gps_latitude
-        #             self.root.ids.mapview.lon = self.gps_longitude
-        #             self.center_map(self.gps_latitude, self.gps_longitude)
-        #             self.use_once = False
-        #     self.update_boat()
-        gps.on_location(self, kwargs)
+        self.gps_latitude = kwargs.get('lat', None) 
+        self.gps_longitude = kwargs.get('lon', None)
+        if hasattr(self,'gps_latitude') and hasattr(self,'gps_longitude'):
+            if self.use_once:
+                if  self.root.ids.mapview.lat and self.root.ids.mapview.lon:
+                    self.root.ids.mapview.lat = self.gps_latitude
+                    self.root.ids.mapview.lon = self.gps_longitude
+                    self.center_map(self.gps_latitude, self.gps_longitude)
+                    self.use_once = False
+            self.update_boat()
     #endregion
     
     def toggle_program(self):
