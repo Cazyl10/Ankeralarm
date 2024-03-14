3809e3fb7bff80f5749e3e745d32ef26fbca179f
Decan29 <58521030+Decan29@users.noreply.github.com>
2024-03-14 14:03:57 +0100

    get_gps und on_location ausgelagert

--- main.py
+++ main.py
@@ -68,29 +68,31 @@ class MainApp(MDApp):
 
     def get_gps(self, *args):
         """Holt GPS-Daten mithilfe von plyer"""
-        if platform == 'android':
-            from plyer import gps   
-            try:                           
-                gps.configure(on_location=self.on_location)
-                gps.start(minTime=100, minDistance=0)
-            except:
-                import traceback
-                traceback.print_exc()
-                self.gps_status= "GPS is not implemented for your platform"
+        # if platform == 'android':
+        #     from plyer import gps   
+        #     try:                           
+        #         gps.configure(on_location=self.on_location)
+        #         gps.start(minTime=100, minDistance=0)
+        #     except:
+        #         import traceback
+        #         traceback.print_exc()
+        #         self.gps_status= "GPS is not implemented for your platform"
+        gps.get_gps(self, args)
 
     def on_location(self, **kwargs):
         """Holt lat und lon Werte des aktuellen Standorts. 
         Zudem fügt es diese beim Start der Mapview hinzu."""
-        self.gps_latitude = kwargs.get('lat', None) 
-        self.gps_longitude = kwargs.get('lon', None)
-        if hasattr(self,'gps_latitude') and hasattr(self,'gps_longitude'):
-            if self.use_once:
-                if  self.root.ids.mapview.lat and self.root.ids.mapview.lon:
-                    self.root.ids.mapview.lat = self.gps_latitude
-                    self.root.ids.mapview.lon = self.gps_longitude
-                    self.center_map(self.gps_latitude, self.gps_longitude)
-                    self.use_once = False
-            self.update_boat()
+        # self.gps_latitude = kwargs.get('lat', None) 
+        # self.gps_longitude = kwargs.get('lon', None)
+        # if hasattr(self,'gps_latitude') and hasattr(self,'gps_longitude'):
+        #     if self.use_once:
+        #         if  self.root.ids.mapview.lat and self.root.ids.mapview.lon:
+        #             self.root.ids.mapview.lat = self.gps_latitude
+        #             self.root.ids.mapview.lon = self.gps_longitude
+        #             self.center_map(self.gps_latitude, self.gps_longitude)
+        #             self.use_once = False
+        #     self.update_boat()
+        gps.on_location(self, kwargs)
     #endregion
     
     def toggle_program(self):
--- src/ankeralarm_gps.py
+++ src/ankeralarm_gps.py
@@ -17,3 +17,29 @@ def permission_callback(self, permissions, results):
             self.get_gps()
         else:
             print("Rechte abgelehnt")
+
+def get_gps(self, *args):
+    """Holt GPS-Daten mithilfe von plyer"""
+    if platform == 'android':
+        from plyer import gps   
+        try:                           
+            gps.configure(on_location=self.on_location)
+            gps.start(minTime=100, minDistance=0)
+        except:
+            import traceback
+            traceback.print_exc()
+            self.gps_status= "GPS is not implemented for your platform"
+
+def on_location(self, **kwargs):
+        """Holt lat und lon Werte des aktuellen Standorts. 
+        Zudem fügt es diese beim Start der Mapview hinzu."""
+        self.gps_latitude = kwargs.get('lat', None) 
+        self.gps_longitude = kwargs.get('lon', None)
+        if hasattr(self,'gps_latitude') and hasattr(self,'gps_longitude'):
+            if self.use_once:
+                if  self.root.ids.mapview.lat and self.root.ids.mapview.lon:
+                    self.root.ids.mapview.lat = self.gps_latitude
+                    self.root.ids.mapview.lon = self.gps_longitude
+                    self.center_map(self.gps_latitude, self.gps_longitude)
+                    self.use_once = False
+            self.update_boat()
